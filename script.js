3b0c106e42b309ddd9a5142c5141c712e7317f3f
maskaram <meskerem185@gmail.com>
2023-08-12 20:41:03 -0400

    js is include

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const create = document.querySelector(".todo_addnew_btn");
+const add = document.querySelector(".todo_add_new_btn");
 const form = document.querySelector(".taskmanager__form");
 const items = document.querySelector(".todo_content_items");
 const cancel = document.querySelector(".cancel");
-
-
+const completed = document.querySelector(".completed");
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     const toggleform=()=>{
-    items.style.display="none"
+    items.style.display="none";
     create.style.display = "none";
-    form.classList.toggle("hide")
-}
-    const cancelform=()=>{
-    items.style.display="block"
-    create.style.display = "block"
-    form.classList.toggle("hide")
+    form.classList.toggle("hide");
+};
+
+    const cancelform=(e)=>{
+    e.preventDefault();
+    items.style.display="block";
+    create.style.display = "block";
+    form.classList.toggle("hide");
 };
 
     create.addEventListener("click",(e)=>{
@@ -22,51 +25,53 @@ const cancel = document.querySelector(".cancel");
 
    cancel.addEventListener("click",(e)=>{
     e.preventDefault();
-    toggleform();
+    cancelform();
 });
 
-const task=JSON.parse(localStorage.getItem("tasks"))||[]
+const tasks=JSON.parse(localStorage.getItem("tasks"))||[]
 console.log(tasks);
 
-const listTask=()=>{
-tasks.forEach((x)=>[
+const listTasks=()=>{
+tasks.forEach((x)=>{
+const {date,description,task_name,task_status,time}=x
     const task=document.createElement("div")
     task.classList.add("todo_content_single");
-    task.innerHTML = `<div
-
-    <div class="todo_content_single_top">
-        <class="flex">
-            <div class="todo_content_single_checkbox complete">
-                <img src="./images/square-regular.svg" class="box"/>
-                <img src="./images/check-solid.svg" 
-                class="right hide"/>
-            </div>
-            <div class="todo_contentsingle_time"></div>2023-06-25--<span class="time">10:55</span></div>
-        
-        <div class="flex">
-            <div class="todo_contentsingle_edit hide"><img src="./images/pen-to-square-solid.svg" alt=""></div>
-            <div class="todo_contentsingle_delete hide"><img src="./images/trash-can-regular.svg" alt=""></div>
-            <div 
-                   
-                          
-    `
-                    
-])
+    task.innerHTML = 
+    `  <div class="todo_content_single ${task_status}">
+                    <div class="todo_contentsingle_top">
+                        <div class="flex">
+                            <div class="todo_contentsingle_checkbox"><img src="./images/check-solid.svg"></div>
+                            <div class="todo_contentsingle_time">${date} <span class="time">${time}</span></div>
+                        </div>
+                        <div class="flex">
+                            <div class="todo_contentsingle_edit hide"><img src="./images/pen-to-square-solid.svg" alt=""></div>
+                            <div class="todo_contentsingle_delete hide"><img src="./images/trash-can-regular.svg" alt=""></div>
+                            <div class="todo_contentsingle_more"><img src="./images/ellipsis-solid.svg" alt=""></div>
+                        </div>
+                    </div>
+                    <div class="todo_contentsingle_title">${task_name}</div>
+                    <div class="todo_contentsingle_description">${description}</div>
+                </div>
+               
+                `  
+                items.appendChild(task)              
+})
 }
 listTasks();
-
-
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
     tasks.push({
     task_name: task_name.value,  
     description: description.value,
-    date:time.value, 
+    date: date.value,
+    time: time.value, 
     id:tasks.length>0?tasks[tasks.length-1].id+1:0,
     task_status:task_status.value,
   });
-  localStorage.setItem("tasks", JSON.stringify(task));
-  toggleForm
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  cancelform(e);
+    listTasks();
+    location.reload();
     
 });
